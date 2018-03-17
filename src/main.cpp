40f96eb828dc28eecbd1cb0b34417120c7373ae0
Artur <kol478@gmail.com>
2018-03-17 06:26:10 +0100

    First function

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @Author: Artur <miszo97>
+ * @Date:   March 17, 2018 5:46 AM
+ * @Email:  artsspe@gmail.com
+ * @Project: Async-Web-Communicator
+ * @Filename: main.cpp
+ * @Last modified by:   miszo97
+ * @Last modified time: March 17, 2018 6:24 AM
+ */
+
+
+
+int main(int argc, char const *argv[]) {
+  // let it start
+  return 0;
+}
