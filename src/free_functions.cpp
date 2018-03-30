eecd60b7ac7299005f4391fbc8ab75f7da01391b
Artur <kol478@gmail.com>
2018-03-30 20:00:51 +0200

    Adding free functions.

---
+++ src/free_functions.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "free_functions.hpp"
+
+void printUsage() noexcept{
+    std::cout << "\nUsage is awc.exe server or,"<< std::endl;
+    std::cout << "awc.exe client IP(to server)\n"<< std::endl;
+}
